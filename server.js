c670aa9f9faaabd39d6637dc66505411424c057d
mirmeherbanali <95076814+mirmeherbanali@users.noreply.github.com>
2025-10-23 22:06:58 +0530

    (feat)-debugger added

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.get("/items", async (req, res) => {
   console.log("inside items route");
   try {
     console.log("before fetch");
-    const items = await Item.find().sort({ createdAt: -1 });
+    // const items = await Item.find().sort({ createdAt: -1 });
+    const items = [{ name: "Test Item", description: "dummy" }];
     console.log("after fetch");
     res.json({ status: 200, data: items });
   } catch (err) {
