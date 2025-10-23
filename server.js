589d281f26bb5a06b0d4f1cb3297adc5e0a70bc6
mirmeherbanali <95076814+mirmeherbanali@users.noreply.github.com>
2025-10-23 12:51:34 +0530

    (feat)-db config updated

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+MONGO_URI="mongodb+srv://chinmaymahantacm_db_user:AxpGl5ErfI9X6GQJ@aihub.k7injzq.mongodb.net/aihub?retryWrites=true&w=majority"
+PORT=4000
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 //comment added
 app.get("/", (req, res) => {
